b7f2593afa701deceb0af9039347228ffafa6e71
ChristopherSemard <christophersemard76@gmail.com>
2022-05-04 10:34:49 +0200

    Nav mobile & fix image cassée

--- app.js
+++ app.js
@@ -97,3 +97,51 @@ let projectsRight = document.querySelectorAll('.project-right')
 for(project of projectsRight){
     observerR.observe(project);
 }
+
+
+// Validation formulaire
+function validate()
+{
+    let firstName = document.getElementById("name").value;
+    let lastName = document.getElementById("lastname").value;
+    let msgContent = document.getElementById("msg").value;
+    let firstNameRGEX = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,100}$/u;
+    let lastNameRGEX = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,100}$/u;
+    let msgContentRGEX = /^[A-Za-z]{1,500}$/
+    let firstNameResult = firstNameRGEX.test(firstName);
+    let lastNameResult = lastNameRGEX.test(lastName);
+    let msgContentResult = msgContentRGEX.test(msgContent);
+    if(firstNameResult == false)
+    {
+        alert('Prénom invalide');
+        return false;
+    }
+    if(lastNameResult == false)
+    {
+        alert('Nom invalide');
+        return false;
+    }
+    if(msgContentResult == false)
+    {
+        alert('Message invalide');
+        return false;
+    }
+
+    return true;
+}
+
+// Menu nav mobile
+function navMobile() {
+    var x = document.querySelector(".nav-mobile-menu");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+      var allNav = document.querySelectorAll(".nav-mobile-link");
+      for(let i = 0 ; i < allNav.length ; i++){
+          allNav[i].classList.add(`nav-anim-${i+1}`);
+      }
+      x.classList.add('nav-mobile-open');
+      
+    }
+  }
